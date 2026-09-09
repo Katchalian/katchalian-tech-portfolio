59769e44547f42b65344392e502e793d6bd9b5d0
Abu Bakr <babakatchaabubakar999@gmail.com>
2026-09-09 21:30:16 +0100

    Initial portfolio

---
+++ script.js
@@ -0,0 +1,68 @@
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-link");
+const scrollTopBtn = document.querySelector(".scroll-top");
+
+window.addEventListener("scroll", () => {
+
+    // Back to Top button
+    if (window.scrollY > 500) {
+        scrollTopBtn.classList.add("show-scroll");
+    } else {
+        scrollTopBtn.classList.remove("show-scroll");
+    }
+
+    // Active navigation
+    let current = "";
+
+    sections.forEach(section => {
+
+        const sectionTop = section.offsetTop - 120;
+        const sectionHeight = section.offsetHeight;
+
+        if (
+            window.scrollY >= sectionTop &&
+            window.scrollY < sectionTop + sectionHeight
+        ) {
+            current = section.getAttribute("id");
+        }
+
+    });
+
+    navLinks.forEach(link => {
+
+        link.classList.remove("active");
+
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+
+    });
+
+});
+
+const menuBtn = document.querySelector(".menu-btn");
+const navMenu = document.querySelector("nav ul");
+
+if (menuBtn) {
+    menuBtn.addEventListener("click", () => {
+        navMenu.classList.toggle("active");
+    });
+}
+
+const viewProjectsBtn = document.querySelector("#view-projects-btn");
+const projectsSection = document.querySelector("#projects");
+
+viewProjectsBtn.addEventListener("click", () => {
+    projectsSection.scrollIntoView({
+        behavior: "smooth"
+    });
+});
+
+const contactMeBtn = document.querySelector("#contact-me");
+const contactSection = document.querySelector("#contact");
+
+contactMeBtn.addEventListener("click", () => {
+    contactSection.scrollIntoView({
+        behavior: "smooth"
+    });
+});
